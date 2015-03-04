bfcd05f263fae050ffc6558ef02fa4eba274354d
Bmewolf <patrick.wolf@duke.edu>
2015-03-04 15:57:46 -0500

    Added some changes to setEngineHardware
    These were used to sync the commands with the C-software.

--- setEngineHardware.m
+++ setEngineHardware.m
@@ -7,10 +7,11 @@ function setEngineHardware(handles)
     sendString = sprintf('~h:E%1d:%1d\n', (handles.SelectedEngine-1), (handles.engine(handles.SelectedEngine).HeadStage-1));
     SendCommandString(handles, sendString);
     % Channel %
-    sendString = sprintf('~c:E%1d:%2d\n', (handles.SelectedEngine-1), (handles.engine(handles.SelectedEngine).Channel-1)); 
+    sendString = sprintf('~c:E%1d:%02d\n', (handles.SelectedEngine-1), (handles.engine(handles.SelectedEngine).Channel-1)); 
     SendCommandString(handles, sendString);
-    % Decimation  - is really samples to skip so 0 is every sample, 1 is every other etc. %
-    sendString = sprintf('~d:E%1d:%3d\n', (handles.SelectedEngine-1), handles.engine(handles.SelectedEngine).Decimation); 
+    % Decimation  - is really samples to skip so 0 is every sample, 1 is
+    % every other etc.  0-99 with leading zeros
+    sendString = sprintf('~d:E%1d:%02d\n', (handles.SelectedEngine-1), handles.engine(handles.SelectedEngine).Decimation); 
     SendCommandString(handles, sendString);
     % Threshold  - Should be sent in unsigned Fixed point 16.12 format as a 4 character hex %
     sendString = sprintf('~T:E%1d:%s\n', (handles.SelectedEngine-1), hex(ufi(handles.engine(handles.SelectedEngine).Threshold, 16, 12))); 
@@ -18,13 +19,13 @@ function setEngineHardware(handles)
     %filter values are sent as a signed 16.15 fixedpoint numbers converted
     %to 4 hex digits
     for i=1:handles.engine(handles.SelectedEngine).nFilterCoef
-        sendString = sprintf('~f:E%1d:%3X %s\n', (handles.SelectedEngine-1), i-1, hex(sfi(handles.engine(handles.SelectedEngine).Filter(i), 16, 15)));
+        sendString = sprintf('~f:E%1d:%03X %s\n', (handles.SelectedEngine-1), i-1, hex(sfi(handles.engine(handles.SelectedEngine).Filter(i), 16, 15)));
         SendCommandString(handles, sendString);
     end
     %template values are sent as a signed 16.15 fixedpoint numbers converted
     %to 4 hex digits
     for i=1:handles.engine(handles.SelectedEngine).nTemplatePoints
-        sendString = sprintf('~t:E%1d:%3X %s\n', (handles.SelectedEngine-1), i-1, hex(sfi(handles.engine(handles.SelectedEngine).Template(i), 16, 15)));
+        sendString = sprintf('~t:E%1d:%03X %s\n', (handles.SelectedEngine-1), i-1, hex(sfi(handles.engine(handles.SelectedEngine).Template(i), 16, 15)));
         SendCommandString(handles, sendString);
     end
     
